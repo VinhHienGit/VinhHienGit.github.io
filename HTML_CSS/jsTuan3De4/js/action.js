a5bbef76e0c6b44d4de28311865e306877f735c3
demorila <hoang.zn99@gmail.com>
2018-10-25 13:52:48 +0700

    update 4

---
+++ HTML_CSS/jsTuan3De4/js/action.js
@@ -0,0 +1,33 @@
+var fallingLeaves = $(".js-falling_leaves");
+var lengthRandom = fallingLeaves.width();
+$(document).ready(function() {
+  var count = 1;
+  var move = 0;
+
+  //create 20 leaf images
+  for (let i = 0; i < 16; i++) {
+    count = Math.ceil(Math.random()*5);
+    fallingLeaves.append("<img src='img/leaves" + count + ".png' class='falling_leaves" + i + "' alt='leaves" + count +"'>");
+  }
+
+  //set action for every leaf per second
+  setInterval(function() {
+    let xStart = Math.ceil(Math.random() * lengthRandom);
+    let xEnd = Math.ceil(Math.random() * lengthRandom);
+    let xSkew = Math.ceil(Math.random() * 1000);
+    let ySkew = Math.ceil(Math.random() * 1000);
+    let time = Math.ceil(Math.random() * 25 + 5);
+
+    TweenMax.fromTo($(".falling_leaves" + move), time, {
+      x: xStart,
+      y: 0,
+    }, {
+      x: xEnd,
+      y: 750,
+      skewX: xSkew,
+      skewY: ySkew,
+      rotation: 200,
+    });
+    move = move < 15 ? ++move : 0;
+  }, 999);
+});
